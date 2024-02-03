b5a9a37fbcf095d5e123a49e8dc367e5da19a49d
Harry Sutherland <harrysutherland17@gmail.com>
2024-02-03 11:13:13 +0000

    fixed card loader definition of last_card needs to be fixed

--- srs/static/srs/learn.js
+++ srs/static/srs/learn.js
@@ -7,9 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
     global_load(new_cards, review_cards)
 });
 
-async function global_load(new_cards, review_cards) {
+function global_load(new_cards, review_cards) {
 
-    await load_new_cards(new_cards, handleLearningComplete);
+    load_new_cards(new_cards);
 
 
 
@@ -17,180 +17,190 @@ async function global_load(new_cards, review_cards) {
 
 }
 
-function load_new_cards(cards, handleLearningComplete) {
-    return new Promise((resolve) => {
+function load_new_cards(cards /*handleLearningComplete*/) {
+    // return new Promise((resolve) => {
 
 
-        let show_button = document.querySelector('#show-answer');
-        let next_card = document.querySelector('#next-card');
-        let fail_button = document.querySelector('#fail-button');
-        let partial_button = document.querySelector('#partial-button');
-        let recalled_button = document.querySelector('#recalled-button');
+    let show_button = document.querySelector('#show-answer');
+    let next_card = document.querySelector('#next-card');
+    let fail_button = document.querySelector('#fail-button');
+    let partial_button = document.querySelector('#partial-button');
+    let recalled_button = document.querySelector('#recalled-button');
 
-        let learning_complete = '';
-        let card_index = 0;
-        let last_card = false;
+    let card_index = 0;
+    let last_card = false;
+    let n_total = 0;
+    let learning_complete = false;
 
 
-        console.log("Cards length: ", cards.length);
+    console.log("Cards length: ", cards.length);
 
-        function update_card() {
-            last_card = false;
-            if (card_index < cards.length) {
-                if (cards[card_index].learnt === false) { // only shows learnt cards
+    function update_card(first_card) {
 
-                }
-                else { // Card is learnt
-                    console.log("Card is learnt");
-                    if (card_index !== cards.length - 1) {
-                        card_index++;
-                    }
-                    else {
-                        card_index = 0;
-                    }
-                }
-
-                show_button.style.display = 'inline';
-                document.querySelector('.front').innerHTML = cards[card_index].front;
-                document.querySelector('.back').innerHTML = '';
-                next_card.style.display = 'none';
-                fail_button.style.display = 'none';
-                partial_button.style.display = 'none';
-                recalled_button.style.display = 'none';
-                console.log("Card index: ", card_index);
-
-            }
-            else {
-                console.error("card_index is out of bounds", card_index);
-                console.log("Number of cards: ", cards.length);
-            }
-
+        function display() {
+            show_button.style.display = 'inline';
+            document.querySelector('.front').innerHTML = cards[card_index].front;
+            document.querySelector('.back').innerHTML = '';
+            next_card.style.display = 'none';
+            fail_button.style.display = 'none';
+            partial_button.style.display = 'none';
+            recalled_button.style.display = 'none';
+            console.log("Card index: ", card_index);
         }
 
-
-
-        function is_complete() {
-            function is_cards_learnt() {
-                for (let i = 0; i < cards.length; i++) {
-                    if (cards[i].learnt !== true) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-
-            if (is_cards_learnt()) {
-                console.log("All cards are learnt");
-                learning_complete = true;
-                document.querySelector('.front').innerHTML = 'Learning stage finished';
-                document.querySelector('.back').innerHTML = '';
-                show_button.style.display = 'none';
-                next_card.style.display = 'none';
-                handleLearningComplete(learning_complete);
-            } else if (card_index === cards.length - 1) {
+        if (card_index < cards.length) {
+            if (card_index === cards.length - 1) {
                 last_card = true;
-                console.log("Last card", card_index);
+            }
+
+            if (cards[card_index].n === 2) {
+                console.log("Card is learnt");
+                if (last_card) { // last card and n = 2
+                    last_card = false;
+                    for (let i = 0; i < cards.length; i++) {
+                        if ((cards[i].n !== 2) && i !== card_index) {
+                            card_index = i;
+                            console.log(i);
+                            break;
+                        }
+
+                    }
+                }
+                else { // not last card and n = 2
+                    card_index++
+                    if (cards[card_index].n === 2) {
+                        for (let i = 0; i < cards.length; i++) {
+
+                            if ((cards[i].n !== 2) && i !== card_index) {
+                                card_index = i;
+                                break;
+                            }
+
+                        }
+                    }
+                }
+                n_total += 2;
+                console.log("n_total: ", n_total)
+
+            }
+            else { // n != 2
+                if (last_card) {
+                    for (let i = 0; i < cards.length; i++) {
+                        if ((cards[i].n !== 2) && i !== card_index) {
+                            card_index = i;
+                            break;
+                        }
+                    }
+                }
+                else if (first_card) {
+                    first_card = false;
+                    card_index = 0;
+                }
+                else {
+                    card_index++;
+                }
+
+            }
+
+            if (n_total === (2 * cards.length)) {
+                console.log("Learning complete");
+                learning_complete = true;
+            }
+            else {
+                display();
             }
         }
+        else {
+            console.error("card_index is out of bounds", card_index);
+            console.log("Number of cards: ", cards.length);
+        }
 
-        update_card(); // show first card
+    }
 
-        show_button.addEventListener('click', () => {
-            if (last_card) {
-                document.querySelector('.back').innerHTML = cards[cards.length - 1].back;
-            }
-            else {
-                document.querySelector('.back').innerHTML = cards[card_index].back;
-            }
+    function is_complete() {
+        if (learning_complete) {
+            document.querySelector('.front').innerHTML = 'Learning stage finished';
+            document.querySelector('.back').innerHTML = '';
             show_button.style.display = 'none';
-            next_card.style.display = 'inline';
-            fail_button.style.display = 'inline';
-            partial_button.style.display = 'inline';
-            recalled_button.style.display = 'inline';
-        })
+            next_card.style.display = 'none';
+            fail_button.style.display = 'none';
+            partial_button.style.display = 'none';
+            recalled_button.style.display = 'none';
+            return true
+        }
+        else {
+            return false;
+        }
+    }
 
-        next_card.addEventListener('click', () => {
-            if (card_index < cards.length - 1) {
-                card_index++;
-            }
-            else {
-                card_index = 0;
-            }
-            update_card();
-            is_complete();
-        })
+    update_card(true); // show first card
 
-        fail_button.addEventListener('click', () => {
+    show_button.addEventListener('click', () => {
+
+        document.querySelector('.back').innerHTML = cards[card_index].back;
+        show_button.style.display = 'none';
+        next_card.style.display = 'inline';
+        fail_button.style.display = 'inline';
+        partial_button.style.display = 'inline';
+        recalled_button.style.display = 'inline';
+    })
+
+    next_card.addEventListener('click', () => {
+        if (card_index < cards.length - 1) {
             card_index++;
-            update_card();
+        }
+        else {
+            card_index = 0;
+        }
+        update_card(false);
+        is_complete();
+    })
+
+    fail_button.addEventListener('click', () => {
+        if (card_index >= 0 && card_index < cards.length) {
+
+            cards[card_index].n = 0;
+            console.log("n: ", cards[card_index].n);
+        }
+
+        update_card(false);
+        is_complete();
+    })
+
+    partial_button.addEventListener('click', () => {
+        if (card_index >= 0 && card_index < cards.length) {
+            if (cards[card_index].n < 0) {
+                cards[card_index].n = cards[card_index].n - 1;
+            }
+
+            console.log("n: ", cards[card_index].n);
+            update_card(false);
             is_complete();
-        })
+        }
+    })
+    recalled_button.addEventListener('click', () => {
+        console.log("recalled");
 
-        partial_button.addEventListener('click', () => {
-            if (card_index >= 0 && card_index < cards.length) {
-                if (cards[card_index].n < 0) {
-                    cards[card_index].n = cards[card_index].n - 1;
-                }
-
-                console.log("n: ", cards[card_index].n);
-
-                if (card_index == cards.length - 1) {
-                    card_index = 0;
-                    console.log("Card index reset");
-
-                }
-                else {
-                    card_index++;
-                    console.log("Card index incremented");
-                }
-                console.log(card_index, cards[card_index].n)
-                update_card();
-                is_complete();
-                update_card();
-                is_complete();
-            }
-        })
-        recalled_button.addEventListener('click', () => {
-            console.log("recalled");
-
-            if (card_index >= 0 && card_index < cards.length) {
+        if (card_index >= 0 && card_index < cards.length) {
+            if (cards[card_index].n < 2) {
                 cards[card_index].n += 1;
-
-                if (cards[card_index].n == 2) {
-                    cards[card_index].i = 1;
-                    cards[card_index].learnt = true;
-                }
-
-                console.log("n: ", cards[card_index].n);
-
-                if (card_index == cards.length - 1) {
-                    card_index = 0;
-                    console.log("Card index reset");
-
-                }
-                else {
-                    card_index++;
-                    console.log("Card index incremented");
-                }
-                update_card();
-                is_complete();
             }
 
-        })
-    });
+            if (cards[card_index].n == 2) {
+                cards[card_index].i = 1;
+                cards[card_index].learnt = true;
+            }
 
-}
-function handleLearningComplete(learning_complete) { // if this is true then the learning stage has been complete
-    if (learning_complete == true) {
-        console.log(learning_complete);
-        return true;
-    }
-    else {
-        console.log(learning_complete);
-        return false;
-    }
-}
+            console.log("n: ", cards[card_index].n);
 
+            update_card(false);
+            is_complete();
+            console.log("n_total", n_total);
+
+        }
+
+    })
+};
 
 
 function format(string) {
